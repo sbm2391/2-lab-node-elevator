4383db9e26690b18ac087fe580c534c5112502d4
Bet Mendoza <sbm2391@hotmail.com>
2018-01-18 09:17:30 -0600

    funciones de log, start stop y update

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,29 @@
 class Elevator {
   constructor(){
     this.floor      = 0;
+    this.direction = "up";
     this.MAXFLOOR   = 10;
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() { 
+    let that = this
+    let interval = setInterval(function(){that.update()}, 1000)
+  }
+  stop() { 
+    clearInterval(this.interval)
+  }
+  update() { 
+    this.log()
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() { 
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
+  }
 }
 
 module.exports = Elevator;
